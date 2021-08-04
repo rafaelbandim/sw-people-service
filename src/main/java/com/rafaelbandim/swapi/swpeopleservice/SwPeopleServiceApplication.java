9dccd26db29b345c1298e4d1d2e629c6b0ff4d01
Rafael Bandim <rafabandim@gmail.com>
2021-08-04 16:20:32 -0700

    first commit

---
+++ src/main/java/com/rafaelbandim/swapi/swpeopleservice/SwPeopleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelbandim.swapi.swpeopleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwPeopleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwPeopleServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelbandim/swapi/swpeopleservice/SwPeopleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelbandim.swapi.swpeopleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwPeopleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
